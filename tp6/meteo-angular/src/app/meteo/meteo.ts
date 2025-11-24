68f4f2cd0473f4c38d3e7eb4d8e3b1e7e7585556
Sunnbro <zidane-88_@Hotmail.com>
2025-11-24 16:53:20 +0100

    pre 7

--- tp6/meteo-angular/src/app/meteo/meteo.ts
+++ tp6/meteo-angular/src/app/meteo/meteo.ts
@@ -6,23 +6,60 @@ import { MeteoItem } from '../meteoItem';
 @Component({
   selector: 'app-meteo',
   standalone: true,
-  imports: [CommonModule, FormsModule], // <-- IMPORTS OBLIGATOIRES
+  imports: [CommonModule, FormsModule],
   templateUrl: './meteo.html',
   styleUrl: './meteo.css'
 })
 export class MeteoComponent {
-  city: MeteoItem = { name: '', weather: '' };
-  cities: MeteoItem[] = [];
+  city: MeteoItem = { id: 0, name: '', weather: null };
+  cityList: MeteoItem[] = [];
 
+  constructor() {
+    this.loadCityList();
+  }
+
+  // Charger la liste depuis le localStorage
+  loadCityList() {
+    const storedList = localStorage.getItem('cityList');
+    if (storedList) {
+      this.cityList = JSON.parse(storedList);
+    } else {
+      this.cityList = [];
+    }
+  }
+
+  // Ajouter une ville
   onSubmit() {
-    if (this.city.name && this.city.name.trim().length >= 3) {
+    if (this.city.name && !this.isCityExist(this.city.name)) {
       const newCity: MeteoItem = {
-        id: this.cities.length + 1,
+        id: this.cityList.length + 1,
         name: this.city.name,
         weather: '☀️'
       };
-      this.cities.push(newCity);
+      this.cityList.push(newCity);
       this.city.name = '';
+      this.saveCityList();
+      console.log(newCity.name, 'ajouté à la liste');
+    } else {
+      console.log(this.city.name, 'existe déjà dans la liste');
     }
+  }
+
+  // Supprimer une ville
+  remove(city: MeteoItem) {
+    this.cityList = this.cityList.filter(item => item.name !== city.name);
+    this.saveCityList();
+  }
+
+  // Vérifier si la ville existe déjà
+  isCityExist(cityName: string): boolean {
+    return this.cityList.some(
+      item => item.name?.toUpperCase() === cityName.toUpperCase()
+    );
+  }
+
+  // Sauvegarder dans localStorage
+  saveCityList() {
+    localStorage.setItem('cityList', JSON.stringify(this.cityList));
   }
 }
